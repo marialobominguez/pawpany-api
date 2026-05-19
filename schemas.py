73c15489d57df802088f52b475cc1c4cc9dc6175
marialobominguez <marialobominguez@gmail.com>
2026-05-19 15:48:17 +0200

    añado los esquemas del resto de tablas

--- schemas.py
+++ schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional
-from models import RolUsuario
+from typing import List, Optional
+from models import EstadoContrato, RolUsuario
+from datetime import datetime, date
 
 # datos del usuario general
 class UsuarioCreate(BaseModel):
@@ -13,7 +14,7 @@ class UsuarioCreate(BaseModel):
     ubicacion: Optional[str] = None
     rol: RolUsuario # el menú creado en models.py
 
-# Esto es lo que la API nos DEVOLVERÁ (sin la contraseña por seguridad)
+# lo que devuelve la API (sin la contraseña por seguridad)
 class UsuarioOut(BaseModel):
     id: int
     username: str
@@ -23,3 +24,93 @@ class UsuarioOut(BaseModel):
 
     class Config:
         from_attributes = True # permite a Pydantic leer datos de SQLAlchemy
+
+# datos de las mascotas
+class MascotaCreate(BaseModel):
+    id_usuario: int
+    nombre: str
+    especie: Optional[str] = None
+    raza: Optional[str] = None
+    fecha_nacimiento: Optional[date] = None
+    requisitos_tags: Optional[List] = None  # al ser JSON en la bbdd, le pido una lista
+    personalidad_libre: Optional[str] = None
+    foto_url: Optional[str] = None
+
+class MascotaOut(BaseModel):
+    id: int
+    id_usuario: int
+    nombre: str
+    especie: Optional[str] = None
+    foto_url: Optional[str] = None
+
+    class Config:
+        from_attributes = True
+
+# datos de los cuidadores
+class PerfilCuidadorCreate(BaseModel):
+    id_usuario: int
+    estudios: str  
+    tarifa: float  
+    cualidades_tags: Optional[List] = None
+    sobre_mi: Optional[str] = None
+    foto_url: Optional[str] = None
+
+class PerfilCuidadorOut(BaseModel):
+    id: int
+    id_usuario: int
+    tarifa: float
+
+    class Config:
+        from_attributes = True
+
+
+# datos de los contratos
+class ContratoCreate(BaseModel):
+    id_dueno: int     
+    id_cuidador: int  
+    fecha_inicio: datetime
+    fecha_fin: datetime
+    precio_total: float
+    detalles_servicio: Optional[str] = None
+    estado: Optional[EstadoContrato] = EstadoContrato.Pendiente # un estado de los de la enumeración en models.py
+
+class ContratoOut(BaseModel):
+    id: int
+    id_dueno: int
+    id_cuidador: int
+    estado: EstadoContrato
+    precio_total: float
+
+    class Config:
+        from_attributes = True
+
+
+# datos de los mensajes
+class MensajeCreate(BaseModel):
+    id_remitente: int     
+    id_destinatario: int  
+    contenido: str
+
+class MensajeOut(BaseModel):
+    id: int
+    id_remitente: int
+    id_destinatario: int
+    contenido: str
+
+    class Config:
+        from_attributes = True
+
+
+# datos de las reseñas
+class ResenaCreate(BaseModel):
+    id_contrato: int  
+    calificacion: int
+    comentario: Optional[str] = None
+
+class ResenaOut(BaseModel):
+    id: int
+    id_contrato: int
+    calificacion: int
+
+    class Config:
+        from_attributes = True
